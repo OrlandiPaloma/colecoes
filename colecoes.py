76cf0f559355cf776b80f96c0d595c298bff96d5
Ubiratan <palomaorlandiw@gmail.com>
2022-06-07 16:27:37 -0300

    colecoes

---
+++ colecoes.py
@@ -0,0 +1,8 @@
+#coleções (listas)
+preco = [20,50,200]
+print(preco[1])
+'''
+Sempre quando quiser citar o que esta dentro da lista, é necessario citar a sua POSIÇÃO. EX: posição do 20 é 0. Posição do 50 é 1 e posição do 200 é 2
+Sempre que quiser procurar qual o indice de um dado dentro da lista, usar o INDEX
+'''
+print(preco.index(200))
